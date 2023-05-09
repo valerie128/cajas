b6be96b71fd6f4fa0629c09330fc661471c70296
valerie128 <124885463+valerie128@users.noreply.github.com>
2023-05-09 10:24:07 -0500

    Add files via upload

---
+++ vale/script.js
@@ -0,0 +1,4 @@
+;<script>
+ var n1,n2,vsuma; n1 = parseInt(prompt('Ingresar numero 1')); n2 =
+ parseInt(prompt('Ingresar numero 2')); vsuma = n1 + n2; document.write('La suma  es '+vsuma);
+</script>
